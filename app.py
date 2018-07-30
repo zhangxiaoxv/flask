383b4dbc5db3d270d989a9e093d69c29f49f98f2
张晓旭 <zhangxiaoxv@hotmail.com>
2018-07-30 23:32:40 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,76 @@
+
+import flask
+import flask_login
+
+app = flask.Flask(__name__)
+app.secret_key = 'super secret string'  # Change this!
+login_manager = flask_login.LoginManager()
+
+login_manager.init_app(app)
+
+
+# Our mock database.
+users = {'test': {'password': '123456'}}
+
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(username):
+    if username not in users:
+        return
+
+    user = User()
+    user.id = username
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('email')
+    if username not in users:
+        return
+
+    user = User()
+    user.id = username
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[username]['password']
+
+    return user
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = flask.request.form['username']
+    if flask.request.form['password'] == users[username]['password']:
+        user = User()
+        user.id = username
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'
+
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
